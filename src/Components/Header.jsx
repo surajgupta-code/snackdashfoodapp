73524f2320f60341446c73ba88c3d493742ae856
surajgupta-code <surajguptanew3@gmail.com>
2026-02-09 21:07:33 +0530

    "fixed error"

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import Logo from "../Assets/logo.png";
+import Logo from "../assets/logo.png";
 import useIsOnline from "../hooks/useIsOnline";
 
 
